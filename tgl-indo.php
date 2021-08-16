4af7b90a5af51917f85017d330b09cb51daaba6b
akptma <engkapratama56@gmail.com>
2021-08-16 09:22:23 +0700

    Konversi Tanggal Indonesia PHP

---
+++ tgl-indo.php
@@ -0,0 +1,54 @@
+<?php
+
+function tgl_indo($val)
+{
+    $explode = explode('-', $val);
+    $tgl     = $explode[0];
+    $bulan   = $explode[1];
+    $tahun   = $explode[2];
+    switch (strtolower($bulan)) {
+        case '01':
+            return ($tgl . ' ' .  'Januari' . ' ' .  $tahun);
+            break;
+        case '02':
+            return ($tgl . ' ' .  'Februari' . ' ' .  $tahun);
+            break;
+        case '03':
+            return ($tgl . ' ' .  'Maret' . ' ' .  $tahun);
+            break;
+        case '04':
+            return ($tgl . ' ' .  'April' . ' ' .  $tahun);
+            break;
+        case '05':
+            return ($tgl . ' ' .  'Mei' . ' ' .  $tahun);
+            break;
+        case '06':
+            return ($tgl . ' ' .  'Juni' . ' ' .  $tahun);
+            break;
+        case '07':
+            return ($tgl . ' ' .  'July' . ' ' .  $tahun);
+            break;
+        case '08':
+            return ($tgl . ' ' .  'Agustus' . ' ' .  $tahun);
+            break;
+        case '09':
+            return ($tgl . ' ' .  'September' . ' ' .  $tahun);
+            break;
+        case '10':
+            return ($tgl . ' ' .  'Oktober' . ' ' .  $tahun);
+            break;
+        case '11':
+            return ($tgl . ' ' .  'November' . ' ' .  $tahun);
+            break;
+        case '12':
+            return ($tgl . ' ' .  'Desember' . ' ' .  $tahun);
+            break;
+
+        default:
+            return "Format bulan harus mengunakan M kecil m, date('d-m-Y H:i:s')";
+            break;
+    }
+}
+
+echo tgl_indo(date('d-m-Y H:i:s'));
+    // output Example:  01 Januari 2022
