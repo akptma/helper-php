5031e50c93cfa14f3e8c4b15fd7200c13316f3b8
akptma <engkapratama56@gmail.com>
2021-08-17 14:51:41 +0700

    Encode & Decode Base64

---
+++ encode-decode-base64.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * $val  = value untuk di decode atau encode
+ * $length = digunakan untuk melakukan looping (encode dan decode nilai lengthnya harus sama)
+ * Examp : 
+ *       -> encode('test', 5) -> value (test) akan di encode sebanyak (10) kali 
+ *       -> decode('VmpKMFYyRXhXWGxXYmxKV1lUQndVbFpyVWtKUFVUMDk', 5) -> value  akan di decode sebanyak (10) kali 
+ * 
+ */
+
+function encode($val, $length)
+{
+    $encode = base64_encode($val);
+    for ($i = 1; $i <= intval($length); $i++) {
+        $encode = base64_encode($encode);
+    }
+    return $encode;
+}
+
+function decode($val, $length)
+{
+    $decode = base64_decode($val);
+    for ($i = 1; $i <= intval($length); $i++) {
+        $decode = base64_decode($decode);
+    }
+    return $decode;
+}
+
+echo encode('Mang Chung', 5);
+echo "<br>";
+echo decode('Vm0xMFlXRXhTWGxWYkdScVVrWmFVMWxVUmt0aU1WVjNWMjFHVGxadFVqRlhXSEJ6VlZaV1ZVMUVhejA9', 5);
